5ab2a43a472856e77f3d13ed362f1fd67ad5de3e
mic-se <none>
2019-01-14 00:12:30 +0100

    mobx version

--- src/App.js
+++ src/App.js
@@ -1,33 +1,27 @@
 import React from 'react';
+import { observer } from 'mobx-react';
 import ActionButton from './ActionButton';
 import './App.css';
 
-
+@observer
 class App extends React.Component {
-  state = { count: 0 };
-
-  minusOperation = () => {
-    const { count } = this.state;
-    if (count === 0) {
-      return;
-    }
-
-    this.setState({ count: count - 1 });
-  };
-
-  plusOperation = () => {
-    const { count } = this.state;
-    this.setState({ count: count + 1 });
-  };
-
   render() {
-    const { count } = this.state;
+    const { store } = this.props;
+    const { count, plusOperation, minusOperation } = store;
     return (
       <div className="flex-container">
         <div className="ui segment row">
           <h1 className="count">{count}</h1>
-          <ActionButton onClick={this.plusOperation} label="+" className="ui button big" />
-          <ActionButton onClick={this.minusOperation} label="-" className="ui button big" />
+          <ActionButton
+            onClick={plusOperation}
+            label="+"
+            className="ui button big"
+          />
+          <ActionButton
+            onClick={minusOperation}
+            label="-"
+            className="ui button big"
+          />
         </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import counterStore from './CounterStore';
 
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<App store={counterStore} />, document.querySelector('#root'));
 
 module.hot.accept();
